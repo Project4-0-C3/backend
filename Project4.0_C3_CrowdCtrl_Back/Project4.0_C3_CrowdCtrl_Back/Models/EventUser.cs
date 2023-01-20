edda5751f1936bca6a4b27e52adb5adc08b22d70
RenzoBeeckmans <r0841635@student.thomasmore.be>
2023-01-20 09:53:56 +0100

    thenincludes voor relational data

--- Project4.0_C3_CrowdCtrl_Back/Project4.0_C3_CrowdCtrl_Back/Models/EventUser.cs
+++ Project4.0_C3_CrowdCtrl_Back/Project4.0_C3_CrowdCtrl_Back/Models/EventUser.cs
@@ -1,11 +1,13 @@
-﻿namespace Project4._0_C3_CrowdCtrl_Back.Models
+﻿using System.Text.Json.Serialization;
+
+namespace Project4._0_C3_CrowdCtrl_Back.Models
 {
     public class EventUser
     {
         public int EventUserId { get; set; }
         public int EventId { get; set; }
         public int UserId { get; set; }
-
+        [JsonIgnore]
         public Event Event { get; set; }
         public User User { get; set; }
     }
